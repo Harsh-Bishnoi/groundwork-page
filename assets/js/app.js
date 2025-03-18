e094bb927de5933e6bf392aa4df5e3b35cf58f5d
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-03-18 14:23:21 +0530

    navbar-mistake solve

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 const navbarbtn = () => {
     document.getElementById("name").classList.toggle("show-navbar")
-    document.getElementById("rotates").classList.toggle("rotate-42")
-    document.getElementById("transparent").classList.toggle("bg-transparent")
-    document.getElementById("rotates-2").classList.toggle("rotate-43")
+    document.getElementById("rotates").classList.toggle("color-orange")
+    document.getElementById("transparent").classList.toggle("color-orange")
+    document.getElementById("rotates-2").classList.toggle("color-orange")
     document.body.classList.toggle("overflow-hidden")
 }
