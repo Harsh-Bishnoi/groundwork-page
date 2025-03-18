ffb1764c8bae0b50b9253a01091b827d808b06ff
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-03-18 15:43:59 +0530

    contact section done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 const navbarbtn = () => {
     document.getElementById("name").classList.toggle("show-navbar")
-    document.getElementById("rotates").classList.toggle("color-orange")
-    document.getElementById("transparent").classList.toggle("color-orange")
-    document.getElementById("rotates-2").classList.toggle("color-orange")
+    document.getElementById("rotates").classList.toggle("color-oranges")
+    document.getElementById("transparent").classList.toggle("color-oranges")
+    document.getElementById("rotates-2").classList.toggle("color-oranges")
     document.body.classList.toggle("overflow-hidden")
 }
